a44fc7ec3d1f31e0fe84801bc14a43c479da068b
dango <a15521474798@outlook.com>
2023-03-10 19:18:11 +0800

    注册中心与远程调用

---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.member.feign;
+
+
+import com.atguigu.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memberCoupons();
+}
