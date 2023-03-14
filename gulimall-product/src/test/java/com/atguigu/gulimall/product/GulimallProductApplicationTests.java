00344c305a90c7cbb6e3809b519661b174c65a5b
dango <a15521474798@outlook.com>
2023-03-14 16:24:35 +0800

    第三方oss服务类 签名认证

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,20 +1,28 @@
 package com.atguigu.gulimall.product;
 
+
+
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
-
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
+
+import javax.annotation.Resource;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @SpringBootTest
 public class GulimallProductApplicationTests {
 
     @Autowired
     private BrandService brandService;
+
+
+
 
     @Test
     public void contextLoads() {
